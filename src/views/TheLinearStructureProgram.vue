46cf9d8adbf79b12c6beb804996b8f87580e8cd5
Skripchenko Anton <skripchenkordp@gmail.com>
2021-01-11 16:54:47 +0300

    add first task

--- src/views/TheLinearStructureProgram.vue
+++ src/views/TheLinearStructureProgram.vue
@@ -1,11 +1,70 @@
 <template>
   <div class="home">
     <h1>Программа линейной структуры</h1>
+
+    <v-form v-model="valid">
+      <v-container>
+        <v-row>
+          <v-col
+                  cols="12"
+                  md="4"
+          >
+            <v-text-field
+                    v-model="len1"
+                    :rules="lenRules"
+                    label="Введите длину для 1 стороны"
+                    required
+            ></v-text-field>
+          </v-col>
+          <v-col
+                  cols="12"
+                  md="4"
+          >
+            <v-text-field
+                    v-model="len2"
+                    :rules="lenRules"
+                    label="Введите длину для 2 стороны"
+                    required
+            ></v-text-field>
+          </v-col>
+          <v-col
+                  cols="12"
+                  md="4"
+          >
+            <v-text-field
+                    v-model="len3"
+                    :rules="lenRules"
+                    label="Введите длину для 3 стороны"
+                    required
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <v-card-title v-if="len1 && len2 && len3">
+          Площадь треугольника: {{square}}
+        </v-card-title>
+      </v-container>
+    </v-form>
   </div>
 </template>
 
 <script>
 export default {
-  name: "TheLinearStructureProgram"
+  name: "TheLinearStructureProgram",
+  data: () => ({
+    valid: false,
+    len1: '',
+    len2: '',
+    len3: '',
+    lenRules: [
+      v => !!v || 'Поле является обязательным'
+    ],
+  }),
+  computed: {
+    square: function () {
+      let semiPerimeter = (this.len1 + this.len2 + this.len3) * 0.5;
+      let result = Math.sqrt(semiPerimeter * (semiPerimeter - this.len1) * (semiPerimeter - this.len2) * (semiPerimeter - this.len3));
+      return result
+    }
+    }
 };
 </script>
